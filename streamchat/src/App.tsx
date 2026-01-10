20a1d79b928e1049d7a194f47542575bb065a054
Saksham <113179290+Saksham131102@users.noreply.github.com>
2026-01-11 00:00:40 +0530

    Basic Setup

---
+++ streamchat/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <main>
+      <div className='p-4 bg-gray-200'>
+        <h1 className='text-2xl font-bold'>Stream<span className="text-red-600">Chat</span></h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
