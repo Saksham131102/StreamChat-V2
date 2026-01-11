2ec2d0b2e4b4798739c3dc1ff55b6c226c6a48d7
Saksham <113179290+Saksham131102@users.noreply.github.com>
2026-01-11 20:31:33 +0530

    Landing Page Implemented

---
+++ streamchat/src/pages/LandingPage/LandingNav.tsx
@@ -0,0 +1,12 @@
+const LandingNav = () => {
+  return (
+    <nav className="p-4">
+      <h1 className="text-2xl font-bold">
+        <span className="text-white">Stream</span>
+        <span className="text-red-500">Chat</span>
+      </h1>
+    </nav>
+  );
+};
+
+export default LandingNav;
