4e712ad864f10d6434d29fec29d01aa6e51b4135
Saksham <113179290+Saksham131102@users.noreply.github.com>
2026-01-21 00:16:30 +0530

    login and signup pages implemented

--- streamchat/src/App.tsx
+++ streamchat/src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/AuthPages/LoginPage/LoginPage";
+import SignupPage from "./pages/AuthPages/SignupPage/SignupPage";
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </main>
   );
--- streamchat/src/contexts/authContext.tsx
+++ streamchat/src/contexts/authContext.tsx
@@ -37,7 +37,7 @@ export const useAuthContext = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
     throw new Error(
-      "useAuthContext must be used within an AuthContextProvider"
+      "useAuthContext must be used within an AuthContextProvider",
     );
   }
   return context;
